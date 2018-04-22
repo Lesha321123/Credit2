633c12af13c4a47191a5203ea6d1d234a86075a0
Lesha321123 <37781150+Lesha321123@users.noreply.github.com>
2018-04-22 22:55:43 +0300

    Calendar

--- src/Credit2.java
+++ src/Credit2.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class Credit2 {
 
     public static void main(String[] args) {
+        //Сегодняшняя дата
+        Calendar data = Calendar.getInstance();
+        SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+        String formatted = format1.format(data.getTime());
+        System.out.println("Сегодня: " +  formatted);
         //Вводим сумму кредита
         System.out.println("Введите сумму кредита");
         double debt = Enter();
@@ -13,19 +20,27 @@ public class Credit2 {
         //Считаем задолженность и обязательный платеж
         while (debt>150) {
             double pay = debt / 100 * mon; //Обязательный платеж
-            if(pay < 150){
+            if(pay > 150){
+                DayOfPayment(data);
+                 }
+            else {
                 pay = 150;
+                DayOfPayment(data);
             }
             System.out.println("Задолженность: " + Math.round(debt));
             System.out.println("Обязательный платеж: " + Math.round(pay));
             debt = debt - pay;
             if(debt < 150){
+                DayOfPayment(data);
                 pay = debt;
+                System.out.println("Задолженность: " + Math.round(debt));
                 System.out.println("Обязательный платеж: " + Math.round(pay));
                 break;
             }
         }
     }
+
+
     //Методы
 
     //Ввод данных
@@ -40,5 +55,11 @@ public class Credit2 {
         double st = sc2.nextDouble();
         return st / 12 + 5;
     }
-
+    //День выплаты
+    public static void DayOfPayment(Calendar data) {
+        data.add(Calendar.DAY_OF_MONTH, 50);
+        SimpleDateFormat format1 = new SimpleDateFormat("dd-MM-yyyy");
+        String formatted = format1.format(data.getTime());
+        System.out.println("Дата выплаты: " +  formatted);
+    }
 }
