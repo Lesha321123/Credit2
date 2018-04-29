70c61b91e71c15690f7722c29d39f10c74ac6222
Lesha321123 <37781150+Lesha321123@users.noreply.github.com>
2018-04-29 12:14:59 +0300

    problem fix

--- src/Frame.java
+++ src/Frame.java
@@ -10,18 +10,18 @@ class NewFrame{
         //название окна
         JFrame MyFrame = new JFrame("Кредит");
         //Размеры окна и координаты
-        MyFrame.setBounds(960, 540, 300, 200);
+        MyFrame.setBounds(960, 540, 400, 200);
         //Закрывать при нажатии
         MyFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //отключение мененджера компоновки
+        //Отключение менеджера компоновки
         MyFrame.setLayout(null);
-        //Отображение окна
-        MyFrame.setVisible(true);
+        //Нельзя изменять размер окна
+        MyFrame.setResizable(false);
 
         //Создание кнопки "Подсчет"
         JButton btn1 = new JButton("Подсчет");
         //Положение и размер кнопки
-        btn1.setBounds(90, 120, 180, 30);
+        btn1.setBounds(190, 120, 180, 30);
 
         //Создание текстового поля "Сумма кредита"
         JTextField tf1 = new JTextField();
@@ -31,19 +31,19 @@ class NewFrame{
         //Создание текстового поля "Процентная ставка"
         JTextField tf2 = new JTextField();
         //Расположение поля
-        tf1.setBounds(230,30, 50,30);
+        tf2.setBounds(230,30, 50,30);
 
 
         //Создание первой текстовой метки
         JLabel jl1 = new JLabel("Сумма кредита");
         //Координаты и размеры метки
-        jl1.setBounds(20,0,150,30);
+        jl1.setBounds(230,0,150,30);
 
 
         //Создание второй текстовой метки
         JLabel jl2 = new JLabel("Процентная ставка");
         //Координаты и размеры метки
-        jl1.setBounds(250,0,150,30);
+        jl2.setBounds(20,0,150,30);
 
 
 
@@ -52,7 +52,8 @@ class NewFrame{
         MyFrame.add(tf2);
         MyFrame.add(jl1);
         MyFrame.add(jl2);
-
+        //Отображение окна
+        MyFrame.setVisible(true);
     }
     }
 
