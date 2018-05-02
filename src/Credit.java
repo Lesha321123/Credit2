dcfc8ab89ce9b92778b7c8772b375892e94be016
Lesha321123 <37781150+Lesha321123@users.noreply.github.com>
2018-05-02 19:01:17 +0300

    delete old forme

--- src/Credit.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class Credit {
-    private JPanel panel1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton посчитатьButton;
-}
